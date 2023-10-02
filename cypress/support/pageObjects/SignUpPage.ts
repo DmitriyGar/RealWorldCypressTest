36504ca557059d4c6c3d0e7ab87848536696132d
DmitriyGar <dimitrobase@gmail.com>
2023-10-02 14:03:51 +0300

    sign up tests

--- cypress/support/pageObjects/SignUpPage.ts
+++ cypress/support/pageObjects/SignUpPage.ts
@@ -3,21 +3,21 @@ export class SignUpPage {
     private getUIelements = {
         logoIcon: () => cy.get('main div div svg'),
         signUpLabel: () => cy.get('main div h1'),
-        firstNameLabel: () => cy.get('#username-label'),
-        firstNameField: () => cy.get('#username'),
-        firstNameError: () => cy.get('#username-helper-text'),
-        lastNameLabel: () => cy.get('#username-label'),
-        lastNameField: () => cy.get('#username'),
-        lastNameError: () => cy.get('#username-helper-text'),
+        firstNameLabel: () => cy.get('#firstName-label'),
+        firstNameField: () => cy.get('#firstName'),
+        firstNameError: () => cy.get('#firstName-helper-text'),
+        lastNameLabel: () => cy.get('#lastName-label'),
+        lastNameField: () => cy.get('#lastName'),
+        lastNameError: () => cy.get('#lastName-helper-text'),
         usernameLabel: () => cy.get('#username-label'),
         usernameField: () => cy.get('#username'),
         usernameError: () => cy.get('#username-helper-text'),
         passwordLabel: () => cy.get('#password-label'),
         passwordField: () => cy.get('#password'),
         passwordError: () => cy.get('#password-helper-text'),
-        confirmPasswordLabel: () => cy.get('#password-label'),
-        confirmPasswordField: () => cy.get('#password'),
-        confirmPasswordError: () => cy.get('#password-helper-text'),
+        confirmPasswordLabel: () => cy.get('#confirmPassword-label'),
+        confirmPasswordField: () => cy.get('#confirmPassword'),
+        confirmPasswordError: () => cy.get('#confirmPassword-helper-text'),
         signUpButton: () => cy.get('form button[data-test="signup-submit"]'),
         haveAnAccountLink: () => cy.get('form div div a'),
         signUpError: () => cy.get('[data-test="signup-error"]'),
@@ -31,6 +31,31 @@ export class SignUpPage {
 
     getSignUpLabel() {
         return this.getUIelements.signUpLabel()
+    }
+    
+
+    getFirstNameLabel() {
+        return this.getUIelements.firstNameLabel()
+    }
+
+    getFirstNameField() {
+        return this.getUIelements.firstNameField()
+    }
+
+    getFirstNameError() {
+        return this.getUIelements.firstNameError()
+    }
+
+    getLastNameLabel() {
+        return this.getUIelements.lastNameLabel()
+    }
+
+    getLastNameField() {
+        return this.getUIelements.lastNameField()
+    }
+
+    getLastNameError() {
+        return this.getUIelements.lastNameError()
     }
 
     getUsernameLabel() {
@@ -55,6 +80,18 @@ export class SignUpPage {
 
     getPasswordField() {
         return this.getUIelements.passwordField()
+    }
+
+    getConfirmPasswordError() {
+        return this.getUIelements.confirmPasswordError()
+    }
+
+    getConfirmPasswordLabel() {
+        return this.getUIelements.confirmPasswordLabel()
+    }
+
+    getConfirmPasswordField() {
+        return this.getUIelements.confirmPasswordField()
     }
 
     getHaveAnAccountLink() {
--- cypress/tests/v2/ui/verifySignUpPage.ts
+++ cypress/tests/v2/ui/verifySignUpPage.ts
@@ -3,24 +3,132 @@ import { Pages } from "../../../support/pageObjects/Pages"
 
 let pages = new Pages();
 
-describe('Verify Sign Up page', () => {
+describe('Verify Sign Up page:', () => {
     beforeEach('Preconditions', () => {
         cy.visit('/')
         pages.signInPage.getDonthaveAnAccountLink().click()
+        pages.signUpPage.getSignUpLabel().should('be.visible')
         cy.wait(500)
     })
 
     it('Verify UI elements presence on Sign Up page', () => {
         pages.signUpPage.getLogo().should('be.visible')
         pages.signUpPage.getSignUpLabel().should('be.visible')
-        pages.signUpPage.getUsernameLabel().should('be.visible')
+        pages.signUpPage.getFirstNameLabel().should('be.visible').and('contain', 'First Name')
+        pages.signUpPage.getFirstNameField().should('be.visible').and('be.empty')
+        pages.signUpPage.getLastNameLabel().should('be.visible').and('contain', 'Last Name')
+        pages.signUpPage.getLastNameField().should('be.visible').and('be.empty')
+        pages.signUpPage.getUsernameLabel().should('be.visible').and('contain', 'Username')
         pages.signUpPage.getUsernameField().should('be.visible').and('be.empty')
-        pages.signUpPage.getPasswordLabel().should('be.visible')
+        pages.signUpPage.getPasswordLabel().should('be.visible').and('contain', 'Password')
         pages.signUpPage.getPasswordField().should('be.visible').and('be.empty')
+        pages.signUpPage.getConfirmPasswordLabel().should('be.visible').and('contain', 'Confirm Password')
+        pages.signUpPage.getConfirmPasswordField().should('be.visible').and('be.empty')
         pages.signUpPage.getHaveAnAccountLink().should('be.visible').invoke('attr', 'href').should('contain', '/signin')
         pages.signUpPage.getSignUpButton().should('be.visible')
         pages.signUpPage.getBuiltByLabel().should('be.visible')
         pages.signUpPage.getBuiltByCypressLogo().should('be.visible')
     })
 
+    it('Verify negative fields validation - first name is empty', () => {
+        pages.signUpPage.getLastNameField().type('test')
+        pages.signUpPage.getUsernameField().type('test')
+        pages.signUpPage.getPasswordField().type('test')
+        pages.signUpPage.getConfirmPasswordField().type('test')
+        pages.signUpPage.getSignUpButton().should('be.disabled')
+    })
+
+    it('Verify negative fields validation - last name is empty', () => {
+        pages.signUpPage.getFirstNameField().type('test')
+        pages.signUpPage.getUsernameField().type('test')
+        pages.signUpPage.getPasswordField().type('test')
+        pages.signUpPage.getConfirmPasswordField().type('test')
+        pages.signUpPage.getSignUpButton().should('be.disabled')
+    })
+
+    it('Verify negative fields validation - username is empty', () => {
+        pages.signUpPage.getFirstNameField().type('test')
+        pages.signUpPage.getLastNameField().type('test')
+        pages.signUpPage.getPasswordField().type('test')
+        pages.signUpPage.getConfirmPasswordField().type('test')
+        pages.signUpPage.getSignUpButton().should('be.disabled')
+    })
+
+    it('Verify negative fields validation - confirm password is empty', () => {
+        pages.signUpPage.getFirstNameField().type('test')
+        pages.signUpPage.getLastNameField().type('test')
+        pages.signUpPage.getUsernameField().type('test')
+        pages.signUpPage.getPasswordField().type('test')
+        pages.signUpPage.getSignUpButton().should('be.disabled')
+    })
+
+    it('Verify negative fields validation - password is empty', () => {
+        pages.signUpPage.getFirstNameField().type('test')
+        pages.signUpPage.getLastNameField().type('test')
+        pages.signUpPage.getUsernameField().type('test')
+        pages.signUpPage.getPasswordField().type('test')
+        pages.signUpPage.getConfirmPasswordField().type('test')
+        pages.signUpPage.getPasswordField().clear()
+        pages.signUpPage.getSignUpButton().should('be.disabled')
+        pages.signUpPage.getPasswordError().should('contain', 'Enter your password')
+        pages.signUpPage.getConfirmPasswordError().should('contain', 'Password does not match')
+    })
+
+
+    it('Verify negative fields validation - password does not math confirm password', () => {
+        pages.signUpPage.getFirstNameField().type('test')
+        pages.signUpPage.getLastNameField().type('test')
+        pages.signUpPage.getUsernameField().type('test')
+        pages.signUpPage.getPasswordField().type('test')
+        pages.signUpPage.getPasswordField().type('Test')
+        pages.signUpPage.getConfirmPasswordField().type('test')
+        pages.signUpPage.getConfirmPasswordError().should('contain', 'Password does not match')
+    })
+
+    it('Verify negative fields validation - password limitation', () => {
+        pages.signUpPage.getFirstNameField().type('test')
+        pages.signUpPage.getLastNameField().type('test')
+        pages.signUpPage.getUsernameField().type('test')
+        pages.signUpPage.getConfirmPasswordField().type('tes')
+        pages.signUpPage.getPasswordField().type('tes')
+        pages.signUpPage.getPasswordError().should('contain', 'Password must contain at least 4 characters')
+        pages.signUpPage.getSignUpButton().should('be.disabled')
+    })
+
+    it('Verify positive fields validation - all fields are filled with MAX limitation of valid data including digits, alphabetical and special characters', () => {
+        pages.signUpPage.getFirstNameField().type('QWERTYtest1~!@#$%^&*()_+')
+        pages.signUpPage.getLastNameField().type('QWERTYtest1~!@#$%^&*()_+')
+        pages.signUpPage.getUsernameField().type('QWERTYtest1~!@#$%^&*()_+')
+        pages.signUpPage.getPasswordField().type('QWERTYtest1~!@#$%^&*()_+')
+        pages.signUpPage.getConfirmPasswordField().type('QWERTYtest1~!@#$%^&*()_+')
+        pages.signUpPage.getFirstNameError().should('not.exist')
+        pages.signUpPage.getLastNameError().should('not.exist')
+        pages.signUpPage.getUsernameError().should('not.exist')
+        pages.signUpPage.getPasswordError().should('not.exist')
+        pages.signUpPage.getConfirmPasswordError().should('not.exist')
+        pages.signUpPage.getSignUpButton().should('be.enabled')
+    })
+
+    it('Verify positive fields validation - all fields are filled with MIN limitation of valid data', () => {
+        pages.signUpPage.getFirstNameField().type('t')
+        pages.signUpPage.getLastNameField().type('T')
+        pages.signUpPage.getUsernameField().type('t')
+        pages.signUpPage.getPasswordField().type('test')
+        pages.signUpPage.getConfirmPasswordField().type('test')
+        pages.signUpPage.getFirstNameError().should('not.exist')
+        pages.signUpPage.getLastNameError().should('not.exist')
+        pages.signUpPage.getUsernameError().should('not.exist')
+        pages.signUpPage.getPasswordError().should('not.exist')
+        pages.signUpPage.getConfirmPasswordError().should('not.exist')
+        pages.signUpPage.getSignUpButton().should('be.enabled')
+    })
+
+    it.only('Verify user can sign up to the app', () => {
+        pages.signUpPage.getFirstNameField().type('testf1')
+        pages.signUpPage.getLastNameField().type('testl1')
+        pages.signUpPage.getUsernameField().type('testu1')
+        pages.signUpPage.getPasswordField().type('test1')
+        pages.signUpPage.getConfirmPasswordField().type('test1')
+        pages.signUpPage.getSignUpButton().should('be.enabled').click()
+    })
 })
